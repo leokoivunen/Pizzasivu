c72cf09089621d3c8f585230f5c1e0e1bf78fdb3
leokoivunen <99177879+leokoivunen@users.noreply.github.com>
2022-03-15 14:04:35 +0200

    create popup menu for order or eat there

--- js/fantasia.js
+++ js/fantasia.js
@@ -1,15 +1,20 @@
 function askUser() {
-    let pop = document.getElementById("popup");
-    pop.style.display = "block";
+  let pop = document.getElementById("popup");
+  pop.style.display = "block";
 
-    let proc = document.getElementById("process");
-    proc.style.display = "none";
-  }
+  let proc = document.getElementById("process");
+  proc.style.display = "none";
+}
 
-  function getOrderDetails() {
-    let pop = document.getElementById("popup");
-    pop.style.display = "none";
+function getOrderDetails() {
+  let form = document.querySelector(".bottom-form");
+  form.style.display = "block";
+}
 
-    let proc = document.getElementById("process");
-    proc.style.display = "block";
-  }
+function closeForm() {
+  let pop = document.getElementById("popup");
+  pop.style.display = "none";
+
+  let proc = document.getElementById("process");
+  proc.style.display = "block";
+}
