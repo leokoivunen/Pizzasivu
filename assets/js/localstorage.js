4db7c8f1d25d27e3f7f2b5dc0b23b75e78ca50bb
leokoivunen <99177879+leokoivunen@users.noreply.github.com>
2022-03-21 14:46:40 +0200

    update login

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -1,20 +1,23 @@
-const register = document.querySelector(".register");
-
-register.addEventListener("click", () => {
-  const fname = document.querySelector(".fname");
-  const lname = document.querySelector(".lname");
+function registerUser(user) {
   const email = document.querySelector(".email");
   const passwd_input = document.querySelector(".passwd");
   const passwd_repeat = document.querySelector(".passwd-repeat");
 
   if (passwd_input.value === passwd_repeat.value) {
-
     localStorage.setItem("Email", "" + email.value);
     localStorage.setItem("Password", "" + passwd_input.value);
 
     alert("Register success!");
-    alert("Redirecting user to new address")
+    alert("Redirecting user to new address");
   } else {
     alert("Syöttämäsi salasana ei ole sama kuin vahvistettava salasana...");
   }
-});
+}
+
+function loginUser(user) {
+  const email = document.querySelector(".email");
+  const password = document.querySelector(".passwd");
+
+  console.log(email.value);
+  console.log(password.value);
+}
