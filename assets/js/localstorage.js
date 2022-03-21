f3e4bccbebec44c69c988c8cb911fc3ad2625dd3
leokoivunen <99177879+leokoivunen@users.noreply.github.com>
2022-03-21 14:14:29 +0200

    update register

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -1,22 +1,21 @@
 const register = document.querySelector(".register");
-let localStorage = [];
 
 register.addEventListener("click", () => {
-  console.log("when button is clicked");
   const fname = document.querySelector(".fname");
   const lname = document.querySelector(".lname");
   const email = document.querySelector(".email");
   const passwd_input = document.querySelector(".passwd");
   const passwd_repeat = document.querySelector(".passwd-repeat");
 
-  if (passwd_input.value.length > 1 && passwd_repeat.value.length > 1) {
-    if (passwd_input.value === passwd_repeat.value) {
-        window.location.href = "http://netmor.netlify.app/kirjautumis.html";
-    } else {
-      alert("Salasana ja salasana uudelleen on väärin!");
-    }
-  }
-  else {
-    alert("Täytä tyhjät tekstilaatikot")
-  }
+  console.log("First Name: " + fname.value);
+  console.log("Last Name: " + lname.value);
+  console.log("Email: " + email.value);
+  console.log("Password: " + passwd_input.value);
+  console.log("Password Repeat: " + passwd_repeat.value);
+
+  // if (passwd_input.value === passwd_repeat.value) {
+  //   window.location.href = "http://netmor.netlify.app/kirjautumis.html";
+  // } else {
+  //   alert("Salasana ja salasana uudelleen on väärin!");
+  // }
 });
